ff82bf4211e172946045d2a383897420272f4a78
f-c-g <f.cg@qq.com>
2018-06-03 22:20:53 +0800

    adding some api

--- Server/Web_Server/distribute.py
+++ Server/Web_Server/distribute.py
@@ -1,7 +1,7 @@
 ########################################################
 # DESCRIPTION:
 # (import app decorator from __init__.py)
-# sort queries and define different responses to them 
+# sort queries and define different responses to them
 # work like a head file
 #
 # AUTHOR: ykx
@@ -12,11 +12,12 @@
 # -*- coding:utf-8 -*-
 
 #from Web_Server import handle_upload
-from Web_Server import app # it means import the variable app from package app
+from Web_Server import app  # it means import the variable app from package app
 from . import handle
 
 from flask import request
-from flask import render_template # render_template() can import the files under the folder 'templates'
+# render_template() can import the files under the folder 'templates'
+from flask import render_template
 
 import json
 import base64
@@ -26,15 +27,16 @@ import urllib
 
 ###################
 
+
 @app.route('/upload/<file_id>/<file_size>', methods=['POST'])
 def myupload(file_id, file_size):
-    #if request.method == 'POST':
-        #data = request.get_data()
-        #print(data)
-        #json_data = json.loads(data.decode("utf-8"))
-        #print(json_data)
-        return handle.upload(file_id, file_size)
-    #else:
+    # if request.method == 'POST':
+    #data = request.get_data()
+    # print(data)
+    #json_data = json.loads(data.decode("utf-8"))
+    # print(json_data)
+    return handle.upload(file_id, file_size)
+    # else:
     #    data = request.get_data()
     #    print(data)
     #    return "get it"
@@ -46,35 +48,32 @@ def myupload(file_id, file_size):
 def home():
     return render_template("index.html")
 
-@app.route('/signin', methods=['GET'])
-def signin_form():
-    return '''<form action="/signin" method="post">
-              <p><input name="username"></p>
-              <p><input name="password" type="password"></p>
-              <p><button type="submit">Sign In</button></p>
-              </form>'''
 
 @app.route('/signin', methods=['POST'])
 def signin():
     # 需要从request对象读取表单内容：
-    if request.form['username']=='admin' and request.form['password']=='password':
+    if request.form['username'] == 'admin' and request.form['password'] == 'password':
         return '<h3>Hello, admin!</h3>'
     return '<h3>Bad username or password.</h3>'
+
 
 @app.route('/user/<username>')
 def show_user_profile(username):
     # show the user profile for that user
     return 'User %s' % username
 
+
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
 
+
 @app.route('/path/<path:subpath>')
 def show_subpath(subpath):
     # show the subpath after /path/
     return 'Subpath %s' % subpath
+
 
 if __name__ == '__main__':
     app.run()
@@ -93,6 +92,6 @@ if __name__ == '__main__':
 # flask & template: http://www.pythondoc.com/flask-mega-tutorial/templates.html
 # flask request: https://www.cnblogs.com/wangjikun/p/6935592.html`
 
-#Deployment Options: http://flask.pocoo.org/docs/1.0/deploying/#deployment
-#Development Server: http://flask.pocoo.org/docs/1.0/server/#server
-#url_for(): http://flask.pocoo.org/docs/1.0/api/#flask.url_for
+# Deployment Options: http://flask.pocoo.org/docs/1.0/deploying/#deployment
+# Development Server: http://flask.pocoo.org/docs/1.0/server/#server
+# url_for(): http://flask.pocoo.org/docs/1.0/api/#flask.url_for
