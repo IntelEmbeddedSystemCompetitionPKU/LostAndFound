5946804f61cb5c6c5b5ce558507fc0d33737a0fb
f-c-g <f.cg@qq.com>
2018-06-08 23:11:39 +0800

    m

--- Server/Web_Server/handle/__init__.py
+++ Server/Web_Server/handle/__init__.py
@@ -1,1 +1,4 @@
 from .handle_upload import *
+from .handle_lost import *
+from .handle_sign import *
+from .handle_qr_code import *
--- Server/Web_Server/handle/handle_qr_code.py
+++ Server/Web_Server/handle/handle_qr_code.py
@@ -1,5 +1,8 @@
 import crypt
 from Crypto.L
+import uuid
+import qrcode
+import json
 
 @app.route('/usr_qr_code', methods=['POST'])
 # 得到附有个人信息的二维码(用户名，密码(MD5，用公钥加密))(Image)
@@ -10,10 +13,14 @@ def get_usr_qr_code():
     passwd=jdata['passwd']
     description=jdata['description']
     try:
-        r=c.execute('select * from user where username="'+username+'" and passwd="'+passwd+'";')
+        r=c.execute('select * from User where username="'+username+'" and passwd="'+passwd+'";')
         if r==0:
             return 'False'
-        qrcode = username+'#'+
+        qrcode = username+'#'+description+'#'+uuid.uuid1().__str__().replace('_','')
+        c.execute('insert into User_qrcode values('+username+','+qrcode+');')
+        # img=qrcode.make(qrcode)
+        # img.save("E:/Some.png")
+        return qrcode
+        # return json.dumps({})
     except:
         return 'False'
-    return 'qrcode_string'
